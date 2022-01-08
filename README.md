5c21a2b6e730fc220ece79ac10e170d89aa820d4
JKpcke <39628199+JKpcke@users.noreply.github.com>
2022-01-08 13:06:31 +0100

    delete

--- README.md
+++
@@ -1,2 +0,0 @@
-# ls01_fi-b-11_johannes_koepcke_rep
- Repository für die Berufsschule
--- Untitled-1.java
+++
@@ -1,7 +0,0 @@
-public public static void main(String[] args) {
-    
-}class Main {
-    public static float name() {
-        
-    }
-}
