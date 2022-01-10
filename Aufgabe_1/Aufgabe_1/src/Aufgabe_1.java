17482fafe438903c567fa9383f91be830fa45601
JKpcke <39628199+JKpcke@users.noreply.github.com>
2022-01-10 19:02:30 +0100

    übertragen von Bitbucket

---
+++ Aufgabe_1/Aufgabe_1/src/Aufgabe_1.java
@@ -0,0 +1,40 @@
+public class Aufgabe_1 {
+    public static void main(String[] args) throws Exception {
+        String we = "In Java fühle ich mich nicht wohl.";
+		String one = "*";
+		String two = "***";
+		String three = "*****";
+		String four = "*******";
+		String five = "*********";
+		String six = "***********";
+		String seven = "***";
+		String eight = "***";
+		String nine = "***";
+		
+		double a =22.4234234;
+		double b =111.2222;
+		double c =4.0;
+		double d =1000000.551;
+		double e =97.34;
+		
+		System.out.print("Hallo ich"+" \"programmiere\"! ");
+		System.out.printf("\n%s\n", we);
+		//Hoffentlich mache ich das richtig.
+
+		System.out.printf("\n%6s\n", one);
+		System.out.printf("%7s\n", two);
+		System.out.printf("%8s\n", three);
+		System.out.printf("%9s\n", four);
+		System.out.printf("%10s\n", five);
+		System.out.printf("%11s\n", six);
+		System.out.printf("%7s\n", seven);
+		System.out.printf("%7s\n", eight);
+		System.out.printf("%7s\n", nine);
+		
+		System.out.printf( "\n%.2f\n" ,     a);
+		System.out.printf( "%.2f\n" ,     b);
+		System.out.printf( "%.2f\n" ,     c);
+		System.out.printf( "%.2f\n" ,     d);
+		System.out.printf( "%.2f\n" ,     e);
+    }
+}
